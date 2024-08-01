1c35146bbeb3cc7589665e164f1248c19b2e041c
Sofie Nordahl Erner <sofie@erner.dk>
2024-08-01 16:16:46 +0100

    Create file depending on file type, now returning value for file being empty

--- src/Additional_funcs.py
+++ src/Additional_funcs.py
@@ -2,21 +2,43 @@
 File containing additional functions which will be used by other scripts
 """
 import os
+import pandas as pd
 
 def check_file(filename,file_type):
 	# Check if file exists and is an 'file_type' file
 	type_2 = "." + file_type
 
-	if not os.path.isfile(filename):
-		print(filename," is not a file")
-		
-		txt_in = input("Create this file? [y/n] ")
-		if txt_in == "y":
-			f = open(filename,"x") # Create file
-			f.close()
-		else:
-			print("Error exiting")
-			exit()
+	empty_file = 0 # Set to 1 if file empty
+
+	# Check file extension
 	if not filename.lower().endswith(type_2):
 		print(filename," is not an ",file_type," file")
 		exit()
+
+	# Check if file exists
+	if not os.path.isfile(filename):
+		print(filename," is not a file")
+		
+		txt_in = input("Create this file? [y/n]")
+		if txt_in == "y":
+			if file_type == "csv":
+				df = pd.DataFrame([])
+				df.to_csv(filename)
+			elif file_type == "xlsx":
+				df = pd.DataFrame([])
+				df.to_excel(filename)
+			else:
+				f = open(filename,"x") # Create file
+				f.close()
+
+			empty_file = 1 # file will now be empty
+		else:
+			print("Error exiting")
+			exit()
+
+	# Check if file empty
+	if os.path.getsize(filename) == 0:
+		empty_file = 1
+
+	# Return file status
+	return empty_file
